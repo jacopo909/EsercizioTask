351396ad545ce5dc4f472e25a0f5a8d9bbd04320
jacopo909 <60032905+jacopo909@users.noreply.github.com>
2021-03-16 19:32:45 +0100

    Add files via upload

---
+++ EsercizioTask/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EsercizioTask
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Multipli(object sender, RoutedEventArgs e)
+        {
+            int n;
+            try
+            {
+                n = Convert.ToInt32(txtNumero);
+                Program p = new Program(n);
+              
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "inserisci un numero...", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+           
+        }
+    }
+}
---
+++ EsercizioTask/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Threading;
+
+
+namespace EsercizioTask
+{
+    public class Program
+    {
+        
+        int NumeriTotali;
+        public Program(int n)
+        {
+            NumeriTotali = n;
+        }
+        static void task()
+        {
+            Task<int> t1 = Task.Factory.StartNew(() => StampaNumeri());
+        }
+        public int StampaNumeri()
+        {          
+            for (int i = 0; i <= 200000000; i = i + 2)
+            {
+                NumeriTotali++;
+            }
+            return NumeriTotali;
+        }
+    }
+}
